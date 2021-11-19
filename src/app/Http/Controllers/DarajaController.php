b92066eeda0ba753d038425177d125704e806f0e
John Irungu <johnydanxin@gmail.com>
2021-11-19 11:55:15 +0300

    Package Development Ready

---
+++ src/app/Http/Controllers/DarajaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Softwarescares\Safaricomdaraja\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DarajaController extends Controller
+{
+    //
+}
---
+++ src/app/Providers/DarajaServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SoftwaresCares\SuperBlog\app\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class DarajaServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
