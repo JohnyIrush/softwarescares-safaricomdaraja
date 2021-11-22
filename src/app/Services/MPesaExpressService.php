d5936ad0df5a6665758819f330b13325301e3714
John Irungu <johnydanxin@gmail.com>
2021-11-22 15:21:50 +0300

    Update MPesaExpressService.php

--- src/app/Services/MPesaExpressService.php
+++ src/app/Services/MPesaExpressService.php
@@ -42,7 +42,7 @@ class MPesaExpressService extends Transaction implements TransactionInterface
         //dd($body);
         $response = json_decode($this->serviceRequest($url, $body));
         //dd($response);
-        if($response && $response->ResponseCode === "0")
+        if($response && $response["ResponseCode"] === "0")
         {
             // Fire Notification
             // dd($response);
